0e3f5ce82e2c4dc6062fd8c42b6e19e5059777a5
beyondkoma <850239158@qq.com>
2014-09-22 00:06:52 +0800

    modify emacs plugin

--- .emacs
+++ .emacs
@@ -1,8 +1,8 @@
-(add-to-list   'load-path  "~/install/emacs-configure-set/")
+(add-to-list   'load-path  "~/work/gitProject/emacsPlugins/emacs-configure-set/")
 
-(load-file   "~/install/emacs-configure-set/emacs-basic-configure.el")
-(load-file   "~/install/emacs-configure-set/emacs-plugin-set.el")
-(load-file   "~/install/emacs-configure-set/emacs-key-binding.el")
+(load-file   "~/work/gitProject/emacsPlugins/emacs-configure-set/emacs-basic-configure.el")
+(load-file   "~/work/gitProject/emacsPlugins/emacs-configure-set/emacs-plugin-set.el")
+(load-file   "~/work/gitProject/emacsPlugins/emacs-configure-set/emacs-key-binding.el")
 
 
 
@@ -23,3 +23,15 @@
 
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/work/toDoList/everydayTodo.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- emacs-configure-set/emacs-plugin-set.el
+++ emacs-configure-set/emacs-plugin-set.el
@@ -1,20 +1,20 @@
 	;;;;yasnippet配置
-	(add-to-list 'load-path  "~/install/emacs-configure-set/yasnippet-master") ;
+	(add-to-list 'load-path  "~/work/gitProject/emacsPlugins/emacs-configure-set/yasnippet-master") ;
 (require 'yasnippet)
 
-(yas/load-directory "~/install/emacs-configure-set/yasnippet-master/snippets")
+(yas/load-directory "~/work/gitProject/emacsPlugins/emacs-configure-set/yasnippet-master/snippets")
 (yas/global-mode 1)
 (yas/minor-mode-on) ; 以minor mode打开，这样才能配合主mode使用g
 
 ;设置auto-complete
-(add-to-list 'load-path "~/install/emacs-configure-set/auto-complete-1.3.1")
+(add-to-list 'load-path "~/work/gitProject/emacsPlugins/emacs-configure-set/auto-complete-1.3.1")
 (require 'auto-complete-config)
 
 (defun ac-yasnippet-candidates ()
 (with-no-warnings       ;; >0.6.0
 (apply 'append (mapcar 'ac-yasnippet-candidate-1 (yas/get-snippet-tables)))))
 
-(add-to-list 'ac-dictionary-directories "~/install/Plugin/auto-complete-1.3.1/dict")
+(add-to-list 'ac-dictionary-directories "~/work/gitProject/emacsPlugins/Plugin/auto-complete-1.3.1/dict")
 (ac-config-default)
 (global-auto-complete-mode t)   ;全局开启auto-complete
 (setq ac-use-quick-help t)     ;显示doc文档信息
@@ -50,19 +50,19 @@ ac-source-filename))
 
 
 
-(add-to-list 'load-path "~/install/emacs-configure-set/wangyinelisp/")
-(load-file "~/install/emacs-configure-set/wangyinelisp/setnu.el")
+(add-to-list 'load-path "~/work/gitProject/emacsPlugins/emacs-configure-set/wangyinelisp/")
+(load-file "~/work/gitProject/emacsPlugins/emacs-configure-set/wangyinelisp/setnu.el")
 (add-hook 'text-mode-hook 'turn-on-setnu-mode)
 
 ;;;;; tabbar mode
-(load-file "~/install/emacs-configure-set/wangyinelisp/tabbar.el")
+(load-file "~/work/gitProject/emacsPlugins/emacs-configure-set/wangyinelisp/tabbar.el")
 (require 'tabbar)
 (tabbar-mode)
 (put 'dired-find-alternate-file 'disabled nil)
 
 
 ;;;;;配置org-mode
-  (setq load-path (cons "~/install/emacs-configure-set/org-8.2.1/lisp"
+  (setq load-path (cons "~/work/gitProject/emacsPlugins/emacs-configure-set/org-8.2.1/lisp"
                     load-path))
   (require 'org-install)
 
@@ -71,7 +71,7 @@ ac-source-filename))
 
 
 ;;;日程管理文件位置
-(setq org-agenda-files (list "~/work/ToDoList/TODO.org" "~/work/ToDoList/everydayTodo.org" ));;从这里读取数据用以让agendaview来进行管理
+(setq org-agenda-files (list "~/work/toDoList/TODO.org" "~/work/toDoList/everydayTodo.org" ));;从这里读取数据用以让agendaview来进行管理
 (setq org-default-notes-file "~/.notes")
 (org-agenda-to-appt);;可以去掉时间戳 以用来定时提醒
 
@@ -79,7 +79,7 @@ ac-source-filename))
 ;;;;;Scheme半结构化编程模式
 ;;;;;M-x paredit-mode 就可以自动载入这个模式。具体的操作方式可以看它的说明（按 C-h m 查看“模式帮助”）
 
-(load-file "~/install/emacs-configure-set/wangyinelisp/paredit.el")
+(load-file "~/work/gitProject/emacsPlugins/emacs-configure-set/wangyinelisp/paredit.el")
 (autoload 'paredit-mode "paredit"
   "Minor mode for pseudo-structurally editing Lisp code."
   t)
@@ -140,12 +140,12 @@ ac-source-filename))
 
 
 ;; xcscope
-(load-file "~/install/emacs-configure-set/xcscope.el")
+(load-file "~/work/gitProject/emacsPlugins/emacs-configure-set/xcscope.el")
 (require 'xcscope)
 
 ;;;web python 
 
-(load-file  "~/install/emacs-configure-set/emacs-for-python/epy-init.el")  ;; install the emacs-for-python
+(load-file  "~/work/gitProject/emacsPlugins/emacs-configure-set/emacs-for-python/epy-init.el")  ;; work/gitProject/emacsPlugins the emacs-for-python
 (epy-setup-checker "pyflakes %f")		       ;configuring the flymake checker
 ;;(epy-django-snippets)				       ;adding the django snippets
 (epy-setup-ipython)				       ;ipyhon integration
@@ -157,4 +157,4 @@ ac-source-filename))
 
 
 ;;;color -theme
-(load-file "~/install/emacs-configure-set/wangyinelisp/koma-theme.el")	;;;非常nice的theme仿vim的我只能说nice！
+(load-file "~/work/gitProject/emacsPlugins/emacs-configure-set/wangyinelisp/koma-theme.el")	;;;非常nice的theme仿vim的我只能说nice！
