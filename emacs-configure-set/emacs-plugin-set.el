c394551b9dd4e47288224ce96ea620642bde045e
beyondkoma <850239158@qq.com>
2016-01-19 19:11:08 +0800

    del unused autocomplete and yanippet  config in the emacs-plugin-set.el

--- emacs-configure-set/emacs-plugin-set.el
+++ emacs-configure-set/emacs-plugin-set.el
@@ -12,55 +12,11 @@
 
 (package-initialize)
 
-;; yasnippet配置
-(add-to-list 'load-path  (concat  Relative-Path  "yasnippet-master")) ;
-(require 'yasnippet)
-
-(yas/load-directory   (concat  Relative-Path "yasnippet-master/snippets"))
-(yas/global-mode 1)
-(yas/minor-mode-on) ; 以minor mode打开，这样才能配合主mode使用
-
-;设置auto-complete
-(add-to-list 'load-path   (concat  Relative-Path "auto-complete-1.3.1"))
-(require 'auto-complete-config)
-
-(defun ac-yasnippet-candidates ()
-(with-no-warnings       ;; >0.6.0
-(apply 'append (mapcar 'ac-yasnippet-candidate-1 (yas/get-snippet-tables)))))
-
-(add-to-list 'ac-dictionary-directories  (concat  Relative-Path "auto-complete-1.3.1/dict"))
-(ac-config-default)
-(global-auto-complete-mode t)   ;全局开启auto-complete
-(setq ac-use-quick-help t)     ;显示doc文档信息
-(setq ac-quick-help-delay 0)
-(setq ac-fuzzy-enable t)     ;输入错误时仍能匹配,需手动触发
-
-(setq-default ac-sources '(ac-source-words-in-same-mode-buffers))     ;添加需要提示的内容
-(setq-default ac-sources '(ac-source-yasnippet
-ac-source-semantic
-ac-source-ropemacs
-ac-source-imenu
-ac-source-functions
-ac-source-symbols
-ac-source-features
-ac-source-dictionary
-ac-source-abbrev
-ac-source-words-in-buffer
-ac-source-files-in-current-dir
-ac-source-filename))
-
-(add-hook 'emacs-lisp-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-symbols)))
-(add-hook 'auto-complete-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-filename)))
 
 
-(set-face-background 'ac-candidate-face "lightgray")
-(set-face-underline 'ac-candidate-face "darkgray")
-(set-face-background 'ac-selection-face "steelblue") ;;; 设置比上面截图中更好看的背景颜色
-(define-key ac-completing-map "\M-n" 'ac-next)  ;;; 列表中通过按M-n来向下移动
-(define-key ac-completing-map "\M-p" 'ac-previous)
-(setq ac-auto-start 0.5)
-(setq ac-dwim t)
-(define-key ac-mode-map (kbd "M-q") 'auto-complete) ;
+
+
+
 
 
 
